3440388492cce39a790de73ecc3d78ed49702595
Balletie <skip_meesie@hotmail.com>
2014-11-04 16:30:16 +0100

    Refactored so that functions operate on all kinds of Universes

--- Main.hs
+++ Main.hs
@@ -8,4 +8,4 @@ main =
   let u = LZ (repeat False) True (repeat False)
     in putStr $ unlines $ take 63 $
        map (map (\x -> if x then '#' else '.') . toList (-72) 73) $
-       iterate (=>> rule) u
+       iterate (=>> rule_90) u
--- Universe.hs
+++ Universe.hs
@@ -2,34 +2,41 @@ module Universe where
 
 import Control.Comonad
 
+class Universe u where
+  left :: u a -> u a
+  right :: u a -> u a
+  u_read :: u a -> a
+  u_write :: a -> u a -> u a
+  u_take :: Int -> u a -> [a]
+
 data ListZipper a = LZ [a] a [a]
 
-left :: ListZipper a -> ListZipper a
-left (LZ (x:xs) y ys) = LZ xs x (y:ys)
+instance Universe ListZipper where
+  left (LZ (x:xs) y ys) = LZ xs x (y:ys)
+  left _ = error "Empty universe"
 
-right :: ListZipper a -> ListZipper a
-right (LZ xs x (y:ys)) = LZ (x:xs) y ys
+  right (LZ xs x (y:ys)) = LZ (x:xs) y ys
+  right _ = error "Empty universe"
 
-listRead :: ListZipper a -> a
-listRead (LZ _ x _) = x
-
-listWrite :: a -> ListZipper a -> ListZipper a
-listWrite x (LZ xs _ ys) = LZ xs x ys
+  u_read (LZ _ x _) = x
+  u_write x (LZ xs _ ys) = LZ xs x ys
+  u_take i (LZ _ x xs) = x : (take (pred i) xs)
 
 instance Functor ListZipper where
   fmap f (LZ xs x ys) = LZ (map f xs) (f x) (map f ys)
 
 instance Comonad ListZipper where
   duplicate a = LZ (tail $ iterate left a) a (tail $ iterate right a)
-  extract = listRead
+  extract = u_read
 
-rule :: ListZipper Bool -> Bool
-rule (LZ (a:_) _ (c:_)) = a ⊕ c
-  where (⊕) = (/=)
+--data Loop a = L [a] a
 
-shift :: Int -> ListZipper a -> ListZipper a
+shift :: (Universe u) => Int -> u a -> u a
 shift i u = iterate (if i < 0 then left else right) u !! abs i
 
-toList :: Int -> Int -> ListZipper a -> [a]
-toList i j u = take (j-i) $ half $ shift i u
-  where half (LZ _ b c) = [b] ++ c
+toList :: (Universe u) => Int -> Int -> u a -> [a]
+toList i j = u_take (j-i) . shift i
+
+rule_90 :: (Universe u) => u Bool -> Bool
+rule_90 u = (u_read . left $ u) ⊕ (u_read . right $ u)
+  where (⊕) = (/=)  -- XOR is the same as not equal
